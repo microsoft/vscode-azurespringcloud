f439d972db94db49b837f71f1655cf070c77d285
Hanxiao Liu <hanli@microsoft.com>
2020-12-16 22:33:38 +0800

    Enable telemetry with telemetry wrapper

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -4,7 +4,8 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { commands } from 'vscode';
-import { AzureTreeItem, IActionContext, openInPortal, openReadOnlyJson, registerCommand } from 'vscode-azureextensionui';
+import { AzureTreeItem, CommandCallback, IActionContext, openInPortal, openReadOnlyJson, registerCommand } from 'vscode-azureextensionui';
+import { instrumentOperation } from 'vscode-extension-telemetry-wrapper';
 import { ext } from '../extensionVariables';
 import { AppInstanceTreeItem } from "../tree/AppInstanceTreeItem";
 import { AppTreeItem } from "../tree/AppTreeItem";
@@ -14,30 +15,38 @@ import { ServiceCommands } from "./ServiceCommands";
 
 // tslint:disable-next-line:export-name
 export function registerCommands(): void {
-    registerCommand("azureSpringCloud.common.loadMore", loadMore);
-    registerCommand("azureSpringCloud.common.refresh", refreshNode);
-    registerCommand("azureSpringCloud.common.openInPortal", openPortal);
-    registerCommand('azureSpringCloud.common.toggleVisibility', AppCommands.toggleVisibility, 250);
-    registerCommand("azureSpringCloud.common.viewProperties", viewProperties);
-    registerCommand("azureSpringCloud.subscription.select", selectSubscription);
-    registerCommand("azureSpringCloud.subscription.createServiceFromPortal", ServiceCommands.createServiceInPortal);
-    registerCommand("azureSpringCloud.service.createApp", ServiceCommands.createApp);
-    registerCommand("azureSpringCloud.service.delete", ServiceCommands.deleteService);
-    registerCommand("azureSpringCloud.app.openPublicEndpoint", AppCommands.openPublicEndpoint);
-    registerCommand("azureSpringCloud.app.openTestEndpoint", AppCommands.openTestEndpoint);
-    registerCommand("azureSpringCloud.app.toggleEndpoint", AppCommands.toggleEndpoint);
-    registerCommand("azureSpringCloud.app.start", AppCommands.startApp);
-    registerCommand("azureSpringCloud.app.stop", AppCommands.stopApp);
-    registerCommand("azureSpringCloud.app.restart", AppCommands.restartApp);
-    registerCommand("azureSpringCloud.app.delete", AppCommands.deleteApp);
-    registerCommand("azureSpringCloud.app.deploy", AppCommands.deploy);
-    registerCommand("azureSpringCloud.app.scale", AppCommands.scale);
-    registerCommand("azureSpringCloud.app.instance.startStreamingLog", AppCommands.startStreamingLogs);
-    registerCommand("azureSpringCloud.app.instance.stopStreamingLog", AppCommands.stopStreamingLogs);
-    registerCommand('azureSpringCloud.app.settings.addSetting', AppCommands.addSetting);
-    registerCommand('azureSpringCloud.app.settings.edit', AppCommands.editSettings);
-    registerCommand('azureSpringCloud.app.setting.edit', AppCommands.editSetting);
-    registerCommand('azureSpringCloud.app.setting.delete', AppCommands.deleteSetting);
+    registerCommandWithTelemetryWrapper('azureSpringCloud.common.loadMore', loadMore);
+    registerCommandWithTelemetryWrapper('azureSpringCloud.common.refresh', refreshNode);
+    registerCommandWithTelemetryWrapper('azureSpringCloud.common.openInPortal', openPortal);
+    registerCommandWithTelemetryWrapper('azureSpringCloud.common.toggleVisibility', AppCommands.toggleVisibility);
+    registerCommandWithTelemetryWrapper('azureSpringCloud.common.viewProperties', viewProperties);
+    registerCommandWithTelemetryWrapper('azureSpringCloud.subscription.select', selectSubscription);
+    registerCommandWithTelemetryWrapper('azureSpringCloud.subscription.createServiceFromPortal', ServiceCommands.createServiceInPortal);
+    registerCommandWithTelemetryWrapper('azureSpringCloud.service.createApp', ServiceCommands.createApp);
+    registerCommandWithTelemetryWrapper('azureSpringCloud.service.delete', ServiceCommands.deleteService);
+    registerCommandWithTelemetryWrapper('azureSpringCloud.app.openPublicEndpoint', AppCommands.openPublicEndpoint);
+    registerCommandWithTelemetryWrapper('azureSpringCloud.app.openTestEndpoint', AppCommands.openTestEndpoint);
+    registerCommandWithTelemetryWrapper('azureSpringCloud.app.toggleEndpoint', AppCommands.toggleEndpoint);
+    registerCommandWithTelemetryWrapper('azureSpringCloud.app.start', AppCommands.startApp);
+    registerCommandWithTelemetryWrapper('azureSpringCloud.app.stop', AppCommands.stopApp);
+    registerCommandWithTelemetryWrapper('azureSpringCloud.app.restart', AppCommands.restartApp);
+    registerCommandWithTelemetryWrapper('azureSpringCloud.app.delete', AppCommands.deleteApp);
+    registerCommandWithTelemetryWrapper('azureSpringCloud.app.deploy', AppCommands.deploy);
+    registerCommandWithTelemetryWrapper('azureSpringCloud.app.scale', AppCommands.scale);
+    registerCommandWithTelemetryWrapper('azureSpringCloud.app.instance.startStreamingLog', AppCommands.startStreamingLogs);
+    registerCommandWithTelemetryWrapper('azureSpringCloud.app.instance.stopStreamingLog', AppCommands.stopStreamingLogs);
+    registerCommandWithTelemetryWrapper('azureSpringCloud.app.settings.addSetting', AppCommands.addSetting);
+    registerCommandWithTelemetryWrapper('azureSpringCloud.app.settings.edit', AppCommands.editSettings);
+    registerCommandWithTelemetryWrapper('azureSpringCloud.app.setting.edit', AppCommands.editSetting);
+    registerCommandWithTelemetryWrapper('azureSpringCloud.app.setting.delete', AppCommands.deleteSetting);
+}
+
+function registerCommandWithTelemetryWrapper(commandId: string, callback: CommandCallback): void {
+    // tslint:disable-next-line:no-any
+    const callbackWithTroubleshooting: CommandCallback = async (context: IActionContext, ...args: any[]) => {
+        return await instrumentOperation(commandId, async () => await callback(context, ...args))();
+    };
+    registerCommand(commandId, callbackWithTroubleshooting);
 }
 
 async function refreshNode(_context: IActionContext, node?: AzureTreeItem): Promise<void> {
--- src/extension.ts
+++ src/extension.ts
@@ -7,46 +7,44 @@
 
 import * as vscode from 'vscode';
 import {
-    AzExtTreeDataProvider,
-    AzureUserInput,
-    callWithTelemetryAndErrorHandling,
-    createApiProvider,
-    createAzExtOutputChannel,
-    IActionContext,
-    registerUIExtensionVariables
+  AzExtTreeDataProvider,
+  AzureUserInput,
+  createApiProvider,
+  createAzExtOutputChannel,
+  registerUIExtensionVariables
 } from 'vscode-azureextensionui';
 import { AzureExtensionApiProvider } from 'vscode-azureextensionui/api';
+import { initializeFromJsonFile, instrumentOperation } from 'vscode-extension-telemetry-wrapper';
 import { registerCommands } from './commands';
 import { ext } from './extensionVariables';
 import { AzureAccountTreeItem } from './tree/AzureAccountTreeItem';
 
-export async function activateInternal(context: vscode.ExtensionContext, perfStats: { loadStartTime: number; loadEndTime: number }, ignoreBundle?: boolean): Promise<AzureExtensionApiProvider> {
-    ext.context = context;
-    ext.ignoreBundle = ignoreBundle;
-    ext.outputChannel = createAzExtOutputChannel('Azure Spring Cloud', ext.prefix);
-    context.subscriptions.push(ext.outputChannel);
-    ext.ui = new AzureUserInput(context.globalState);
-
-    registerUIExtensionVariables(ext);
-
-    await callWithTelemetryAndErrorHandling('azureSpringCloud.activate', async (activateContext: IActionContext) => {
-        activateContext.telemetry.properties.isActivationEvent = 'true';
-        activateContext.telemetry.measurements.mainFileLoad = (perfStats.loadEndTime - perfStats.loadStartTime) / 1000;
-
-        const accountTreeItem: AzureAccountTreeItem = new AzureAccountTreeItem();
-        context.subscriptions.push(accountTreeItem);
-        ext.tree = new AzExtTreeDataProvider(accountTreeItem, 'azureSpringCloud.common.loadMore');
-        context.subscriptions.push(vscode.window.createTreeView('azureSpringCloud', {
-            treeDataProvider: ext.tree,
-            showCollapseAll: true,
-            canSelectMany: true
-        }));
-        registerCommands();
-    });
-
-    return createApiProvider([]);
+export async function activateInternal(context: vscode.ExtensionContext, ignoreBundle?: boolean): Promise<AzureExtensionApiProvider> {
+  await initializeFromJsonFile(context.asAbsolutePath('./package.json'), { firstParty: true });
+  await instrumentOperation('activation', activateExtension)(context, ignoreBundle);
+  return createApiProvider([]);
 }
 
 export function deactivateInternal(): void {
-    ext.diagnosticWatcher?.dispose();
+  ext.diagnosticWatcher?.dispose();
+}
+
+async function activateExtension(_operationId: string, context: vscode.ExtensionContext, ignoreBundle?: boolean): Promise<void> {
+  ext.context = context;
+  ext.ignoreBundle = ignoreBundle;
+  ext.outputChannel = createAzExtOutputChannel('Azure Spring Cloud', ext.prefix);
+  context.subscriptions.push(ext.outputChannel);
+  ext.ui = new AzureUserInput(context.globalState);
+
+  registerUIExtensionVariables(ext);
+
+  const accountTreeItem: AzureAccountTreeItem = new AzureAccountTreeItem();
+  context.subscriptions.push(accountTreeItem);
+  ext.tree = new AzExtTreeDataProvider(accountTreeItem, 'azureSpringCloud.common.loadMore');
+  context.subscriptions.push(vscode.window.createTreeView('azureSpringCloud', {
+    treeDataProvider: ext.tree,
+    showCollapseAll: true,
+    canSelectMany: true
+  }));
+  registerCommands();
 }
