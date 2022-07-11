6597a175d2c532fd273c8b30fc6005dd6a7e5e99
Mingliang Wang <wangmi@microsoft.com>
2022-07-11 15:16:19 +0800

    replace file head of each source file.

--- src/commands/steps/creation/SelectAppStackStep.ts
+++ src/commands/steps/creation/SelectAppStackStep.ts
@@ -1,7 +1,5 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
 
 import { KnownSupportedRuntimeValue } from "@azure/arm-appplatform";
 import { AzureWizardPromptStep, IAzureQuickPickItem } from "@microsoft/vscode-azext-utils";
--- src/service/streamlog/streamingLog.ts
+++ src/service/streamlog/streamingLog.ts
@@ -1,7 +1,5 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
 import { DeploymentResource, TestKeys } from '@azure/arm-appplatform';
 import { BasicAuthenticationCredentials, WebResource } from "@azure/ms-rest-js";
 import { parseError } from '@microsoft/vscode-azext-utils';
