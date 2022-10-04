e25f292a98626c3709531eedeb086aba824a0085
Mingliang Wang <wangmi@microsoft.com>
2022-10-04 14:54:26 +0800

    fix the buffer head(channel) issue, which cause connection closed after handle-shake.

--- src/service/remotedebugging/DebugProxy.ts
+++ src/service/remotedebugging/DebugProxy.ts
@@ -16,42 +16,13 @@ export class DebugProxy extends EventEmitter {
     private _wsconnection: websocket.connection | undefined;
     private readonly _instance: EnhancedInstance;
     private readonly _port: number;
+    private messagesRead: number = 0;
 
     constructor(instance: EnhancedInstance, port: number) {
         super();
         this._instance = instance;
         this._port = port;
         this._server = createServer();
-    }
-
-    public async newStart(): Promise<void> {
-        if (!this._server) {
-            this.emit('error', new Error('Proxy server is not started.'));
-        } else {
-            const deployment: EnhancedDeployment = this._instance.deployment;
-            const credential: { accessToken: string } = await deployment.app.service.subscription.credentials.getToken();
-            const appName: string = deployment.app.name;
-            const deploymentName: string = deployment.name;
-            const instanceName: string = this._instance.name ?? 'unknown-instance';
-            const fqdn: string = deployment.app.properties?.fqdn ?? 'unknown-host';
-            const url: string = `wss://${fqdn}/api/remoteDebugging/apps/${appName}/deployments/${deploymentName}/instances/${instanceName}?port=5005`;
-            const token: string = credential.accessToken;
-            const forward = new PortForward();
-            this._server.on('connection', async (socket: Socket) => {
-                forward.portForward(url, token, socket);
-            });
-
-            this._server.on('listening', () => {
-                ext.outputChannel.appendLog('[Proxy Server] start listening');
-                this.emit('start');
-            });
-
-            this._server.listen({
-                host: '127.0.0.1',
-                port: this._port,
-                backlog: 1
-            });
-        }
     }
 
     public async start(): Promise<void> {
@@ -73,8 +44,7 @@ export class DebugProxy extends EventEmitter {
                         ext.outputChannel.appendLog('[WebSocket] client connected');
                         this._wsconnection = connection;
 
-                        connection.on('close', (code: number, desc: string) => {
-                            console.log('close:', code, desc);
+                        connection.on('close', () => {
                             ext.outputChannel.appendLog('[WebSocket] client closed');
                             this.dispose();
                             socket.destroy();
@@ -82,16 +52,25 @@ export class DebugProxy extends EventEmitter {
                         });
 
                         connection.on('error', (err: Error) => {
-                            console.log('error:', err);
                             ext.outputChannel.appendLog(`[WebSocket] ${err}`);
                             this.dispose();
                             socket.destroy();
                             this.emit('error', err);
                         });
 
-                        connection.on('message', (data: websocket.Message) => {
-                            if ('binaryData' in data && data.binaryData) {
-                                socket.write(data.binaryData);
+                        connection.on('message', (msg: websocket.Message) => {
+                            this.messagesRead = this.messagesRead + 1;
+                            if (this.messagesRead > 2 && 'binaryData' in msg && msg.binaryData) {
+                                const channel: number = msg.binaryData.readUInt8(0);
+                                const data: Buffer = msg.binaryData.slice(1);
+                                if (channel === 1) {
+                                    ext.outputChannel.appendLog(`[WebSocket] Received an error: ${data.toString()}`);
+                                    this.dispose();
+                                    socket.destroy();
+                                } else {
+                                    ext.outputChannel.appendLog(`[WebSocket] Received: ${data.toString()}`);
+                                    socket.write(data);
+                                }
                             }
                         });
                         socket.resume();
@@ -111,7 +90,7 @@ export class DebugProxy extends EventEmitter {
                     const instanceName: string = this._instance.name ?? 'unknown-instance';
                     const fqdn: string = deployment.app.properties?.fqdn ?? 'unknown-host';
                     this._wsclient.connect(
-                        `wss://remote-debugging-tooling.azuremicroservices.io/api/remoteDebugging/apps/debug-test-1/deployments/default/instances/debug-test-1-default-12-6ffc755876-hf67k?port=5005`,
+                        `wss://${fqdn}/api/remoteDebugging/apps/${appName}/deployments/${deploymentName}/instances/${instanceName}?port=5005`,
                         undefined,
                         undefined,
                         {
@@ -123,7 +102,9 @@ export class DebugProxy extends EventEmitter {
 
                     socket.on('data', (data: Buffer) => {
                         if (this._wsconnection) {
-                            this._wsconnection.send(data);
+                            const channel: Buffer = Buffer.from([0]);
+                            ext.outputChannel.appendLog(`[Proxy Server] Sent: ${data.toString()}`);
+                            this._wsconnection.send(Buffer.concat([channel, data]));
                         }
                     });
 
