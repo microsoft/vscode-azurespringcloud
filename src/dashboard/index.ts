ea2c10f5ca44e12ce5bb579f810e7ff06c181026
Yan Zhang <yanzh@microsoft.com>
2023-02-13 16:12:56 +0800

    feat: can show app in dashboard
    Signed-off-by: Yan Zhang <yanzh@microsoft.com>

--- src/dashboard/index.ts
+++ src/dashboard/index.ts
@@ -1,45 +1,70 @@
 import * as vscode from "vscode";
-import { DashboardExtensionApi, RemoteBootAppDataProvider, RemoteBootAppDataProviderOptions } from "./api";
+import { AppTreeItem } from "../tree/AppTreeItem";
+import { DashboardExtensionApi, RemoteBootAppData, RemoteBootAppDataProvider, RemoteBootAppDataProviderOptions } from "./api";
+
+let inited: boolean = false;
 
 export async function init(context: vscode.ExtensionContext) {
+    if (inited) {
+        return;
+    }
+
     const dashboardExt = vscode.extensions.getExtension<DashboardExtensionApi>("vscjava.vscode-spring-boot-dashboard");
     if (dashboardExt) {
+        await vscode.commands.executeCommand("setContext", "spring.dashboard:enabled", true);
+
         const api = await dashboardExt.activate(); // TODO: passively get API instead of forcing to activate it?
+        const provider = new AzureSpringAppsProvider(context);
         const options: RemoteBootAppDataProviderOptions = { iconPath: new vscode.ThemeIcon("azure") };
-        const iconPathForApps = {
+        api.registerRemoteBootAppDataProvider("Azure", provider, options);
+
+        vscode.commands.registerCommand("azureSpringApps.app.showLiveInformation", (appNode: any) => {
+            provider.addAppData(appNode);
+            vscode.commands.executeCommand("spring.apps.focus");
+        });
+        inited = true;
+    } else {
+        // TODO: ask user consent to install dashboard extension?
+    }
+}
+
+class AzureSpringAppsProvider implements RemoteBootAppDataProvider {
+
+    store: Map<string, RemoteBootAppData>;
+
+    iconPathForApps: { light: string | vscode.Uri; dark: string | vscode.Uri };
+    onDidChangeDataEmitter: vscode.EventEmitter<void>;
+
+    onDidChangeData: vscode.Event<void>;
+    constructor(context: vscode.ExtensionContext) {
+        this.store = new Map();
+        this.iconPathForApps = {
             dark: vscode.Uri.joinPath(context.extensionUri, "resources", "dark", "app.svg"),
             light: vscode.Uri.joinPath(context.extensionUri, "resources", "light", "app.svg"),
         };
+        this.onDidChangeDataEmitter = new vscode.EventEmitter<void>();
+        this.onDidChangeData = this.onDidChangeDataEmitter.event;
+    }
 
-        const emitter = new vscode.EventEmitter<void>();
-        vscode.commands.registerCommand("azure.springcloud.changedata", () => emitter.fire());
-        const provider: RemoteBootAppDataProvider = {
-            onDidChangeData: emitter.event,
-            provide() {
-                // TODO: provide a list of running spring app metadata
-                return [
-                    {
-                        host: "localhost-8080",
-                        jmxurl: "http://localhost:8080/actuator",
-                        name: "appRunningOnLocalhost",
-                        description: "via actuator http endpoints",
-                        group: "azure-spring-cloud",
-                        iconPath: iconPathForApps
-                    },
-                    {
-                        host: "hanli-test-spring-app-spring-app-20230207162021.azuremicroservices.io",
-                        jmxurl: "https://hanli-test-spring-app-spring-app-20230207162021.azuremicroservices.io/actuator",
-                        name: "hanli-test-spring-app-spring-app-20230207162021",
-                        description: "spring-petclinic",
-                        group: "azure-spring-cloud",
-                        iconPath: iconPathForApps
-                    }
-                ];
-            },
+    provide(): RemoteBootAppData[] | Thenable<RemoteBootAppData[]> {
+        return Array.from(this.store.values());
+    }
+
+    public addAppData(appNode: any) {
+        const appData = this.toRemoteBootAppData(appNode);
+        this.store.set(appData.name, appData);
+        this.onDidChangeDataEmitter.fire();
+    }
+
+    private toRemoteBootAppData(appNode: AppTreeItem): RemoteBootAppData {
+        const app = appNode.app;
+
+        return {
+            name: app.name,
+            host: app.name,
+            description: app.service.name,
+            jmxurl: app.properties?.url + "/actuator",
+            iconPath: this.iconPathForApps
         };
-
-        api.registerRemoteBootAppDataProvider("Azure", provider, options);
-    } else {
-        // TODO: ask user consent to install dashboard extension?
     }
 }
